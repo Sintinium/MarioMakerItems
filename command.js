bf6a05fca031d65d25a4baf430ea13c4a66228f6
Sintinium <sintinium@gmail.com>
2024-09-14 18:44:21 -0500

    Updated to be actually random, oops

--- command.js
+++ command.js
@@ -1,14 +1,18 @@
-$(eval
-  
-info = "Copy everything into a Custom Command where it says 'Message'. List of items is on the next line";
+$(eval 
+
 items = $(urlfetch json https://raw.githubusercontent.com/Sintinium/MarioMakerItems/main/items.json);
 
 itemCount = 2;
 arg1 = $(1);
 if (arg1 != null && arg1.toString().match(/^[2-9]+$/) != null) { 
     itemCount = parseInt(arg1); 
-} 
-shuffled = items.sort(() => 0.5 - Math.random()).slice(0, itemCount);
+}
+picked = [];
+for (var i = 0; i < itemCount; i++) {
+  rand = Math.floor(Math.random() * items.length);
+  picked.push(items[rand]);
+  items.splice(rand, 1);
+}
 
-'@$(user) Which will be next? "' + shuffled.join('" or "') + '"';
+'@$(user) Which will be next? "' + picked.join('" or "') + '"';
 )
