4dec3d8e5f0ed1ffda9fa01150f1797d56540189
Sintinium <sintinium@gmail.com>
2024-09-14 21:42:25 -0500

    Changed so if there's multiple items it doesn't say 'or' between all of them

--- command.js
+++ command.js
@@ -2,17 +2,19 @@ $(eval
 
 items = $(urlfetch json https://raw.githubusercontent.com/Sintinium/MarioMakerItems/main/items.json);
 
-itemCount = 2;
+count = 2;
 arg1 = $(1);
-if (arg1 != null && arg1.toString().match(/^[2-9]+$/) != null) { 
-    itemCount = parseInt(arg1); 
+if (arg1 != null) { 
+    count = parseInt(arg1); 
+    if (isNaN(count)) count = 2;
 }
 picked = [];
-for (var i = 0; i < itemCount; i++) {
+for (var i = 0; i < count; i++) {
   rand = Math.floor(Math.random() * items.length);
   picked.push(items[rand]);
   items.splice(rand, 1);
 }
 
-'@$(user) Which will be next? "' + picked.join('" or "') + '"';
+msg = picked.join("', '").replace(/, ([^,]*)$/, ' or $1'); 
+"@$(user) Which will be next? '" + msg + "'"; 
 )
