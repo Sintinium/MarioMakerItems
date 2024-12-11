eb7aaf8eb4dba389b92194cd6c25a1e3449322c9
Sintinium <sintinium@gmail.com>
2024-12-10 19:46:51 -0600

    Create randomize.js

---
+++ randomize.js
@@ -0,0 +1,20 @@
+function shuffle(array) {
+  let currentIndex = array.length;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+
+    // Pick a remaining element...
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+  return array;
+}
+
+fetch('https://raw.githubusercontent.com/Sintinium/MarioMakerItems/main/items.json')
+  .then(response => response.json())
+  .then(response => console.log(JSON.stringify(shuffle(response), null, 4)));
