ba0110d6fa49a82e0eebab8ca41dfad4521be425
Sintinium <sintinium@gmail.com>
2026-06-29 02:46:00 -0500

    Switched to cloudflare worker due to nightbot issues

--- command.js
+++ command.js
@@ -1,10 +1,1 @@
-$(eval 
-
-items = $(urlfetch json https://raw.githubusercontent.com/Sintinium/MarioMakerItems/main/items.json);
-
-picked = [];
-for (var i = 0; i < Math.min(2, items.length); i++) {
-  picked.push("'" + items.splice(Math.floor(Math.random() * items.length), 1)[0] + "'");
-}
-"@$(user) Which will be next? " + new Intl.ListFormat('en', {style: 'long', type: 'disjunction'}).format(picked); 
-)
+@$(user) $(urlfetch https://bald.sintinium.workers.dev)
